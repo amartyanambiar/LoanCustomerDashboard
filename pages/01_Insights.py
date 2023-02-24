31d037e5eff0adf073aab0cc1e0a4704a8256d31
amartyanambiar <amartyanambiar@gmail.com>
2023-02-24 12:09:33 +0530

    feat: create 01_Insights.py and '02_Additional Insights.py'

---
+++ pages/01_Insights.py
@@ -0,0 +1,168 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+portfolio_data = pd.read_csv('data/Portfolio_data.csv')
+communication_history = pd.read_csv('data/CommunicationHistory.csv')
+merged_df = pd.merge(portfolio_data, communication_history, on="Loan Number")
+
+titles = ['1. Distribution of loans across different states', "2. Distribution of loans across different amounts", "3. Distribution of customers by age",
+          "4. Distribution of loans by due dates", "5. Unique number of customers contacted on daily basis", "6. Number of customers not contacted on daily basis", "7. Who are the customers who have been contacted the most", "8. Who are the customers who have been contacted the least", "9. How many customers have never been reached out?", "10. Distribution of calls by states.", "11. Distribution of calls by campaign ids", "12. Unique number of customers by campaign ID"]
+st.title('Loan Customer Dashboard')
+
+
+st.sidebar.title('12 Findings')
+page = st.sidebar.multiselect(
+    'Select/Deselect the findings you want to see/unsee', titles, default=titles)
+
+
+if titles[0] in page:
+    st.subheader(titles[0])
+    loans_by_state = portfolio_data.groupby(
+        'State')['Loan Number'].count().sort_values(ascending=False)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write(loans_by_state)
+    with col2:
+        st.bar_chart(loans_by_state.head(
+            10))
+    st.write('- **Maharashtra** has the highest number of loans. Other states topping the list are **Tamil Nadu, Karnataka & Telangana**')
+    st.write('- **Pondicherry, Mizoram, Madhya Pradesh, Chattisgarh, Haryana** has the least number of loans')
+    st.write('- States in the South have the highest number of loans.')
+    st.write('')
+
+if titles[1] in page:
+    st.subheader(titles[1])
+
+    fig = px.histogram(portfolio_data, x="Loan Amount", nbins=50,
+                       title="Distribution of loans across different amounts")
+    fig.update_xaxes(title_text='Loan Amount')
+    fig.update_yaxes(title_text='Count')
+    st.plotly_chart(fig)
+    st.write(
+        '- Most of the loans can be approximated to be in the range of **2500 to 7500**')
+    st.write('')
+
+if titles[2] in page:
+    st.subheader(titles[2])
+    portfolio_data['Age'] = pd.to_datetime(
+        portfolio_data['DOB'], errors='coerce').dt.year
+    portfolio_data['Age'] = 2020 - portfolio_data['Age']
+
+    fig = px.histogram(portfolio_data, x="Age", nbins=100,
+                       title="Distribution of customers by age")
+    fig.update_xaxes(title_text='Age')
+    fig.update_yaxes(title_text='Count')
+    st.plotly_chart(fig)
+    st.write(
+        '- Age group of **20 - 26** have the highest number of loans - **1000+**')
+    st.write('- Loans are provided to teens as well of age **16 +**')
+    st.write('')
+
+if titles[3] in page:
+    st.subheader(titles[3])
+    portfolio_data['Due Date'] = pd.to_datetime(
+        portfolio_data['Due Date'], errors='coerce').dt.date
+    portfolio_data['Due Date'] = pd.to_datetime(
+        portfolio_data['Due Date'], errors='coerce').dt.day
+    fig = px.histogram(portfolio_data, x="Due Date", nbins=100,
+                       title="Distribution of loans by due dates")
+    fig.update_xaxes(title_text='Due Date')
+    fig.update_yaxes(title_text='Count')
+    st.plotly_chart(fig)
+    st.write(
+        "- A huge number of the loans are due on the **first 5 days** of the month")
+    st.write("- Many Loans are due on the **19 - 25 of the month** as well")
+    st.write('')
+
+if titles[4] in page:
+    st.subheader(titles[4])
+    completed_calls = communication_history[communication_history['Status'] == 'completed']
+    completed_calls['Date'] = pd.to_datetime(
+        completed_calls['Call time'], errors='coerce').dt.date
+    unique_customers_contacted = completed_calls.groupby(
+        'Date')['Loan Number'].nunique()
+    unique_customers_contacted = unique_customers_contacted.to_frame()
+    unique_customers_contacted.columns = ['Number of customers contacted']
+    st.write(unique_customers_contacted)
+    st.write("")
+
+if titles[5] in page:
+    st.subheader(titles[5])
+    completed_calls = communication_history[communication_history["Status"] == "completed"]
+    completed_calls["Call time"] = pd.to_datetime(completed_calls["Call time"])
+    completed_calls["Call Date"] = completed_calls["Call time"].dt.date
+    unique_customers_contacted = completed_calls.groupby([completed_calls["Call Date"], "Loan Number"])[
+        "Loan Number"].count().reset_index(name="Count")
+    total_customers = len(communication_history["Loan Number"].unique())
+
+    customers_not_contacted = {}
+    for date in unique_customers_contacted["Call Date"].unique():
+        customers_contacted = unique_customers_contacted[unique_customers_contacted["Call Date"] == date]
+        customers_not_contacted[str(
+            date)] = total_customers - len(customers_contacted)
+    NotContactedFrame = pd.DataFrame.from_dict(
+        customers_not_contacted, orient='index', columns=['Number of customers not contacted'])
+    st.write(NotContactedFrame)
+    st.write("")
+
+
+if titles[6] in page:
+    st.subheader(titles[6])
+    most_contacted_customers = communication_history.groupby(
+        'Loan Number')['Loan Number'].count().sort_values(ascending=False)
+    most_contacted_customers = most_contacted_customers.to_frame()
+    most_contacted_customers.columns = ['Number of times contacted']
+    most_contacted_customers = most_contacted_customers.head(10)
+    st.write(most_contacted_customers)
+    st.write('- These are the top 10 customers who were contacted the most')
+    st.write('- **Loan No. 1,659,081** applicant was contacted **38 times**')
+    st.write("")
+
+if titles[7] in page:
+    st.subheader(titles[7])
+    least_contacted_customers = communication_history.groupby(
+        'Loan Number')['Loan Number'].count().sort_values(ascending=True)
+    least_contacted_customers = least_contacted_customers[least_contacted_customers == 1]
+    least_contacted_customers = least_contacted_customers.to_frame()
+    least_contacted_customers.columns = ['Number of times contacted']
+    st.write("Around **"+str(len(least_contacted_customers)) +
+             "** customers were contacted only once")
+    st.write(least_contacted_customers)
+
+if titles[8] in page:
+    st.subheader(titles[8])
+    customers_never_contacted = portfolio_data[~portfolio_data['Loan Number'].isin(
+        communication_history['Loan Number'].unique())]['Loan Number'].count()
+    st.write("**"+str(customers_never_contacted) +
+             '** Customers were never contacted')
+    st.write('')
+
+if titles[9] in page:
+    st.subheader(titles[9])
+    st.bar_chart(
+        merged_df['State'].value_counts().sort_values(ascending=False))
+    st.write('- The highest number of calls are from the state of **Maharashtra**')
+    st.write('')
+
+
+if titles[10] in page:
+    st.subheader(titles[10])
+    calls_by_campaign = communication_history.groupby('Campaign ID')[
+        'Loan Number'].count().sort_values(ascending=False)
+    calls_by_campaign = calls_by_campaign.to_frame()
+    calls_by_campaign.columns = ['Number of calls']
+    st.write(calls_by_campaign)
+    st.write('- Most of the calls are from **Campaign 164,168,595**')
+    st.write('- Least number of calls are from **Campaign 135,474,249**')
+    st.write('')
+
+
+if titles[11] in page:
+    st.subheader(titles[11])
+    unique_customers_by_campaign = communication_history.groupby('Campaign ID')[
+        'Loan Number'].nunique().sort_values(ascending=False)
+    st.write(unique_customers_by_campaign)
+    st.write(
+        '- Most of the customers have been targeted in the **Campaign 164,168,595**')
+    st.write('- Least number of customers have been targeted in **Campaign 161,307,935** and **Campaign 135,474,249**')
---
+++ pages/02_Additional Insights.py
@@ -0,0 +1,88 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import matplotlib.pyplot as plt
+
+portfolio_data = pd.read_csv('data/Portfolio_data.csv')
+communication_history = pd.read_csv('data/CommunicationHistory.csv')
+merged_df = pd.merge(portfolio_data, communication_history, on="Loan Number")
+
+
+st.title('Additional Insights')
+
+st.subheader('1. States with highest "completed" calls')
+completed_calls = merged_df[merged_df['Status'] == 'completed']
+completed_calls = completed_calls.groupby(
+    'State')['Loan Number'].count().sort_values(ascending=False)
+completed_calls = completed_calls.to_frame()
+completed_calls.columns = ['Number of completed calls']
+col1, col2 = st.columns(2)
+with col1:
+    st.write(completed_calls)
+with col2:
+    st.bar_chart(completed_calls.head(10))
+st.write('- **Maharashtra** has the highest number of completed calls')
+st.write('- **Madhya Pradesh** has the least number of completed calls')
+st.write("")
+
+st.subheader('2. States with highest "failed" calls')
+failed_calls = merged_df[merged_df['Status'] == 'FAILED']
+failed_calls = failed_calls.groupby(
+    'State')['Loan Number'].count().sort_values(ascending=False)
+failed_calls = failed_calls.to_frame()
+failed_calls.columns = ['Number of failed calls']
+col1, col2 = st.columns(2)
+with col1:
+    st.write(failed_calls)
+with col2:
+    st.bar_chart(failed_calls.head(10))
+st.write('- Only **9 States** have failed calls')
+st.write('- **Maharashtra** has the highest number (5) of failed calls. Considering the high number of loans in Maharashtra, this is not a significant number')
+st.write('- **West Bengal** has the least number of failed calls')
+st.write('- Focus on Delhi. It has very high number of failed calls in comparison to the total loans')
+st.write("")
+
+st.subheader('3. Select Campaign ID to see the "Age" of customers targeted')
+campaign_id = st.selectbox(
+    'Select Campaign ID', merged_df['Campaign ID'].unique())
+campaign_id_df = merged_df[merged_df['Campaign ID'] == campaign_id]
+campaign_id_df['Age'] = pd.to_datetime(
+    campaign_id_df['DOB'], errors='coerce').dt.year
+campaign_id_df['Age'] = 2020 - campaign_id_df['Age']
+fig = px.histogram(campaign_id_df, x="Age", nbins=100,
+                   title=f"Distribution of customers by age for Campaign ID {campaign_id}")
+fig.update_xaxes(title_text='Age')
+fig.update_yaxes(title_text='Count')
+st.plotly_chart(fig)
+st.write('- Campaigns like **166790104** and **167104482** have a high number of customers in the age group of **20-24**')
+st.write('- Campaigns like **167104481** have targeted customers in the ages of **24, 32**')
+st.write("")
+
+
+st.subheader('4. Select Campaign ID to see the "State" of customers targeted')
+campaign_id1 = st.selectbox(
+    'Select Campaign ID', merged_df['Campaign ID'].unique(), key=f"state")
+campaign_id_df = merged_df[merged_df['Campaign ID'] == campaign_id1]
+state_counts = campaign_id_df["State"].value_counts()
+fig = px.bar(x=state_counts.index, y=state_counts.values, color=state_counts.index,
+             labels={'x': 'State', 'y': 'Number of Customers'},
+             title=f'State Distribution for Campaign ID {campaign_id1}')
+st.plotly_chart(fig)
+st.write('- Campaigns like **166790104** have targeted many states. Prioiritised states like **Maharashtra, Karnataka* & *Tamil Nadu**')
+st.write('- Campaigns **167104481** has not been very successful in targeting customers in states with high number of loans like **Maharashtra, Karnataka & Tamil Nadu**')
+st.write("")
+
+
+st.subheader("5. Loan Amount vs Age Analysis")
+merged_df['Age'] = pd.to_datetime(
+    merged_df['DOB'], errors='coerce').dt.year
+merged_df['Age'] = 2020 - merged_df['Age']
+fig = px.scatter(merged_df, x="Age", y="Loan Amount",
+                 title="Loan Amount vs Age Analysis")
+fig.update_xaxes(title_text='Age')
+fig.update_yaxes(title_text='Loan Amount')
+st.plotly_chart(fig)
+st.write('- **Age** does seem to have a significant impact on the **Loan Amount**')
+st.write('- In the age group of **16-25**, we can see a consistent increase in the **Loan Amount**')
+st.write('- And then the plot flattens out, then the Loan Amount drops significantly after the approximate age of **38-40** in most cases')
